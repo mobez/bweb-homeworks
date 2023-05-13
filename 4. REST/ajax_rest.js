be75c939a3c7da3aaca70abbb2699d5f10c26e77
mobez <aanik@bk.ru>
2023-05-14 01:41:42 +1000

    Task4

--- 4. REST/ajax_rest.js
+++ 4. REST/ajax_rest.js
@@ -3,8 +3,8 @@ $(document).ready(function () {
     var formData = {
       query: $("#ip").val(),
     };
-	var url = "";
-	var token = "";
+	var url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/iplocate/address?ip=";
+	var token = "a0334f2b906868d9a15c7318d7d03906c4aab011";
 
     $.ajax({
       type: "GET",
@@ -16,7 +16,11 @@ $(document).ready(function () {
       dataType: "json",
       encode: true,
     }).done(function (result) {
-      console.log(result);
+        let reshtml = `<pre>Страна: ${result.location.data.country}\r`;
+        reshtml += `Регион: ${result.location.data.federal_district}\r`;
+        reshtml += `Город: ${result.location.data.city}</pre>`;
+        $('#result').html(reshtml);
+        console.log(result);
 	});
 
     event.preventDefault();
